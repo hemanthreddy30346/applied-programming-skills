e65a0ec670e7551b973d0792a48a1f7efd0a17f2
hemanthreddy30346 <vtu30346@veltech.edu.in>
2026-08-25 14:41:04 +0530

    Time: 5 ms (85.95%), Space: 46.3 MB (87.03%) - LeetHub

---
+++ 0641-design-circular-deque/0641-design-circular-deque.java
@@ -0,0 +1,71 @@
+class MyCircularDeque {
+
+    int[] deque;
+    int front;
+    int size;
+    int capacity;
+
+    public MyCircularDeque(int k) {
+        deque = new int[k];
+        capacity = k;
+        front = 0;
+        size = 0;
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) return false;
+
+        front = (front - 1 + capacity) % capacity;
+        deque[front] = value;
+        size++;
+
+        return true;
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+
+        int rear = (front + size) % capacity;
+        deque[rear] = value;
+        size++;
+
+        return true;
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+
+        front = (front + 1) % capacity;
+        size--;
+
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+
+        size--;
+        return true;
+    }
+
+    public int getFront() {
+        if (isEmpty()) return -1;
+
+        return deque[front];
+    }
+
+    public int getRear() {
+        if (isEmpty()) return -1;
+
+        int rear = (front + size - 1) % capacity;
+        return deque[rear];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+}
